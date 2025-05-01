33b7d06b95e49e756c5d70c78913d3e1cd200116
Oleh Levchenko <leva13007@gmail.com>
2025-05-01 20:46:18 +0100

    01 create IPhone frame

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className="App">
+      <div className="IPhone-case">
+        <div className="IPhone-stuff">
+          <div className="IPhone-tail"></div>
+          <div className="IPhone-buttons">
+            <div className="IPhone-button"></div>
+            <div className="IPhone-button"></div>
+            <div className="IPhone-button"></div>
+          </div>
+        </div>
+        <div className="calculator">
+          <div className="display"></div>
+          <div className="keyboard"></div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
