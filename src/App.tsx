1074fad961ba19d102f5e14889fdec2c3c9c2c4d
Oleh Levchenko <leva13007@gmail.com>
2025-05-01 21:13:21 +0100

    02 Create keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,48 @@ function App() {
         </div>
         <div className="calculator">
           <div className="display"></div>
-          <div className="keyboard"></div>
+          <div className="keyboard">
+            <button className="key controls">AC</button>
+            <button className="key controls">
+              <i className="fa-solid fa-square-root-variable"></i>
+            </button>
+            <button className="key controls">
+              x<sup>y</sup>
+            </button>
+            <button className="key operations">
+              <i className="fa-solid fa-divide"></i>
+            </button>
+
+            <button className="key number">7</button>
+            <button className="key number">8</button>
+            <button className="key number">9</button>
+            <button className="key operations">
+              <i className="fa-solid fa-xmark"></i>
+            </button>
+
+            <button className="key number">5</button>
+            <button className="key number">6</button>
+            <button className="key number">7</button>
+            <button className="key operations">
+              <i className="fa-solid fa-minus"></i>
+            </button>
+
+            <button className="key number">1</button>
+            <button className="key number">2</button>
+            <button className="key number">3</button>
+            <button className="key operations">
+              <i className="fa-solid fa-plus"></i>
+            </button>
+
+            <button className="key number">
+              <i className="fa-solid fa-calculator"></i>
+            </button>
+            <button className="key number">0</button>
+            <button className="key number">.</button>
+            <button className="key operations">
+              <i className="fa-solid fa-equals"></i>
+            </button>
+          </div>
         </div>
       </div>
     </main>
