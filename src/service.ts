a8e45fb8f510a37d9fa70239c07ef54c9e306a9d
Oleh Levchenko <leva13007@gmail.com>
2025-05-02 00:32:18 +0100

    08-2 SYA - check priority

--- src/service.ts
+++ src/service.ts
@@ -2,7 +2,7 @@ const toPostfix = (expression: string): string[] => {
   const output: string[] = [];
   const stack: string[] = [];
 
-  const operators = {
+  const operators: Record<string, { priority: number }> = {
     "-": { priority: 1 },
     "+": { priority: 1 },
     "*": { priority: 2 },
@@ -16,7 +16,13 @@ const toPostfix = (expression: string): string[] => {
   for (const token of tokens) {
     if (!isNaN(Number(token))) {
       output.push(token);
-    } else {
+    } else if (token in operators) {
+      while (
+        stack.length &&
+        operators[stack[stack.length - 1]].priority >= operators[token].priority
+        ) {
+        output.push(stack.pop()!);
+      }
       stack.push(token);
     }
   }
